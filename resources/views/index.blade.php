5a36b10bd06925dafceadb23c4246cc20e305424
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-01-28 20:11:50 -0300

    link no titulo do site

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,7 +12,9 @@
     <nav class="bg-black text-white p-4">
         <div class="container mx-auto flex justify-between items-center">
             <div class="flex items-center space-x-6">
-                <h1 class="text-2xl font-bold text-red-600">The News</h1>
+                <h1 class="text-2xl font-bold text-red-600">
+                    <a href="{{ route('home') }}" ">The News</a>
+                </h1>
                 <button class="md:hidden focus:outline-none" id="menu-btn">
                     <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
