f5eb805731e0582009dfe400f571c382fac1add1
Matheus Silveira <matheus.silveiraw@gmail.com>
2025-01-30 00:25:33 -0300

    atualizado buscar para inglês

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,7 +34,7 @@
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 104.5 4.5a7.5 7.5 0 0012.15 12.15z"></path>
                     </svg>
                 </button>
-                <input id="search-input" type="text" placeholder="Buscar..." class="hidden absolute right-0 top-0 px-3 py-1 rounded focus:outline-none focus:ring-2 focus:ring-red-500 text-black">
+                <input id="search-input" type="text" placeholder="Search..." class="hidden absolute right-0 top-0 px-3 py-1 rounded focus:outline-none focus:ring-2 focus:ring-red-500 text-black">
             </div>
         </div>
         <div class="md:hidden hidden p-4 space-y-2 bg-black text-white" id="mobile-menu">
